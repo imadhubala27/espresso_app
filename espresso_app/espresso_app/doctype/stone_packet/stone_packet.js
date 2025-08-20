0624b208b012e172a47ac68163eb4ec600d9e173
imadhubala27 <imadhubala27@gmail.com>
2025-08-20 16:28:59 +0530

    customizations

--- espresso_app/espresso_app/doctype/stone_packet/stone_packet.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, espresso_app and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Stone Packet", {
-// 	refresh(frm) {
-
-// 	},
-// });
